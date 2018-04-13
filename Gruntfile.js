212986abadd30206c30c927acf36fd272241fca4
Colin von Grimmenstein <Colinv@rocketbuild.com>
2018-04-13 11:58:42 -0400

    - Add Simon Said - Add Favicons - Update OG content

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,27 +44,28 @@ module.exports = function (grunt) {
                 options: {
                     template: 'src/html/portfolio_item.mustache'
                 }, files: {
-                    'portfolio/rockstar.html': 'src/config/portfolio/rockstar.json',
-                    'portfolio/realty.html': 'src/config/portfolio/realty.json',
-                    'portfolio/paintballer.html': 'src/config/portfolio/paintballer.json',
-                    'portfolio/klipsch.html': 'src/config/portfolio/klipsch.json',
-                    'portfolio/napa.html': 'src/config/portfolio/napa.json',
-                    'portfolio/radio.html': 'src/config/portfolio/radio.json',
+                    'portfolio/aim.html': 'src/config/portfolio/aim.json',
+                    'portfolio/audiobit_xchange.html': 'src/config/portfolio/audiobit_xchange.json',
                     'portfolio/cityscape.html': 'src/config/portfolio/cityscape.json',
+                    'portfolio/cityway.html': 'src/config/portfolio/cityway.json',
                     'portfolio/generic.html': 'src/config/portfolio/generic.json',
                     'portfolio/gicc.html': 'src/config/portfolio/gicc.json',
-                    'portfolio/speedloader.html': 'src/config/portfolio/speedloader.json',
-                    'portfolio/macl.html': 'src/config/portfolio/macl.json',
-                    'portfolio/cityway.html': 'src/config/portfolio/cityway.json',
-                    'portfolio/audiobit_xchange.html': 'src/config/portfolio/audiobit_xchange.json',
-                    'portfolio/triphase.html': 'src/config/portfolio/triphase.json',
-                    'portfolio/safe-visitor.html': 'src/config/portfolio/safe-visitor.json',
-                    'portfolio/aim.html': 'src/config/portfolio/aim.json',
                     'portfolio/indy-connect.html': 'src/config/portfolio/indy-connect.json',
-                    'portfolio/oobeo.html': 'src/config/portfolio/oobeo.json',
+                    'portfolio/ipl.html': 'src/config/portfolio/ipl.json',
                     'portfolio/junket.html': 'src/config/portfolio/junket.json',
+                    'portfolio/klipsch.html': 'src/config/portfolio/klipsch.json',
+                    'portfolio/macl.html': 'src/config/portfolio/macl.json',
+                    'portfolio/napa.html': 'src/config/portfolio/napa.json',
+                    'portfolio/oobeo.html': 'src/config/portfolio/oobeo.json',
+                    'portfolio/paintballer.html': 'src/config/portfolio/paintballer.json',
                     'portfolio/prairie-godmothers.html': 'src/config/portfolio/prairie-godmothers.json',
-                    'portfolio/ipl.html': 'src/config/portfolio/ipl.json'
+                    'portfolio/radio.html': 'src/config/portfolio/radio.json',
+                    'portfolio/rockstar.html': 'src/config/portfolio/rockstar.json',
+                    'portfolio/realty.html': 'src/config/portfolio/realty.json',
+                    'portfolio/safe-visitor.html': 'src/config/portfolio/safe-visitor.json',
+                    'portfolio/simon-said.html': 'src/config/portfolio/simon-said.json',
+                    'portfolio/speedloader.html': 'src/config/portfolio/speedloader.json',
+                    'portfolio/triphase.html': 'src/config/portfolio/triphase.json'
                 }
             }
         },
